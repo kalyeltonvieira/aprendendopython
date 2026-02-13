8d4ec0b7fc1d86898d1ff2405cbac5e7fd8388ca
kalyeltonvieira <kalyeltonvieira@gmail.com>
2026-02-13 19:25:46 -0300

    Update ex077.py

---
+++ exercicios/ex077.py
@@ -0,0 +1,17 @@
+lista = ('APRENDER', 'CURSO', 'PYTHON', 'GUANABARA', 'PROGRAMAR', 'LINGUAGEM', 'GRATIS', 'ESTUDAR',
+         'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+vogais = 'AEIOU'
+
+for palavra in lista:
+    print(f'\nNA PALAVRA {palavra}: ', end='')
+    
+    tem_vogal = False
+
+    for letra in palavra:
+        if letra in vogais:
+            print(letra, end=' ')
+            tem_vogal = True
+
+    if not tem_vogal:
+        print('NAO TEM VOGAIS')
+
