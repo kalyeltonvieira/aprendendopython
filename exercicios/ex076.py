a7958cff681f4574ecdd8b86370c8ac9d1ea3b78
kalyeltonvieira <kalyeltonvieira@gmail.com>
2026-02-13 19:04:41 -0300

    Update ex076.py

--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -1,3 +1,6 @@
+print('=' * 40)
+print(f'{"LISTAGEM DE PRECOS":^40}')
+print('=' * 40)
 listagem = ('lapis', 1.75,
             'Borracha', 2,
             'Caderno', 15.90,
@@ -8,4 +11,7 @@ listagem = ('lapis', 1.75,
             'Canetas', 22.30,
             'Livro', 34.90)
 for pos in range(0, len(listagem)):
-    print(listagem[pos])
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:.>7.2f}')
