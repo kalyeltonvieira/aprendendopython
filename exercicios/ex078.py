553b7272a31d0bc1d6913c763d8f8c8d445136aa
kalyeltonvieira <kalyeltonvieira@gmail.com>
2026-02-15 16:01:21 -0300

    update

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -1,4 +1,17 @@
-valores = [6,2,7,3,9,4]
-valores.sort()
-valores.sort(id)
-print(valores)
+valor = []
+for c in range(1,6):
+    valor.append(int(input(f'DIGITE O {c} VALOR: ')))
+print(f'OS VALORES LISTADOS FORAM {valor}')
+
+maior = max(valor)
+menor = min(valor)
+
+print(f'O MAIOR VALOR FOI {maior} NA POSIÇÂO ', end='')
+for i, v in enumerate(valor):
+    if v == maior:
+        print(f'{i+1}...', end='')
+
+print(f'\nO MENOR VALOR FOI {menor} NA POSIÇÃO ', end='')
+for i, v in enumerate(valor):
+    if v == menor:
+        print(f'{i+1}...', end='')
